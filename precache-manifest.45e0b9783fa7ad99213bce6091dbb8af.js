c080346c43205709035f27ffdff718394ac0b927
Niger Little-Poole <nlittlepoole@gmail.com>
2019-02-02 17:56:50 -0500

    Updates

---
+++ precache-manifest.45e0b9783fa7ad99213bce6091dbb8af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d15edccbab817ebfde",
+    "url": "/taringando/static/css/main.cbd1e787.chunk.css"
+  },
+  {
+    "revision": "60d15edccbab817ebfde",
+    "url": "/taringando/static/js/main.60d15edc.chunk.js"
+  },
+  {
+    "revision": "f4496d6a81dace3b9bb5",
+    "url": "/taringando/static/js/1.f4496d6a.chunk.js"
+  },
+  {
+    "revision": "64ac5f678ab6174fc34b",
+    "url": "/taringando/static/js/runtime~main.64ac5f67.js"
+  },
+  {
+    "revision": "60c074bf173caab674c2ebfca0c564c5",
+    "url": "/taringando/index.html"
+  }
+];
